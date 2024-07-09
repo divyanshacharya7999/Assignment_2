42a9dc3aa66b1cfa8f6e695ac62e2b9f5f48a5d5
Divyansh Acharya <priyanshacharya@Divyanshs-Macbook-Air.local>
2024-07-09 11:17:05 +0530

    add new feature in file

--- Ass_3.py
+++ Ass_3.py
@@ -3,7 +3,17 @@ class Assignment:
   def __init__(self, Character ="Bittu", age = 20):
     self.Character = Character
     self.age = age
+
+  def character(self, value):
+    self.character = value
+
+  def age(self, value):
+    self.age = value
+    
+  def display(self):
     print(f"{self.Character} is {self.age} years old.")
    
 a = Assignment() 
+a.display()
 b = Assignment("Divyansh","20")
+b.display()
